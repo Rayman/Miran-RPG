20f1c36d70319c924582c6e5129914f1f8cf6f0e
Ramon Wijnands <rayman747@hotmail.com>
2010-02-01 19:32:56 +0100

    Modified the login page

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,1 +1,10 @@
-Welcom to Miran. On the left side you can login.
+Welcom to Miran. On the left side you can login.<br />
+<br />
+If you want, you can take a look with our beta tester account:<br />
+user: test<br />
+pass: test<br />
+<br />
+Feel free to go to the <a href="<?=site_url('register')?>">registration</a> page to register a new account.<br />
+<br />
+Thank,<br />
+Miran Staff<br />
